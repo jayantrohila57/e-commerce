6311b491e52d2859c91fc97d2a2685c3c5ee8add
Jayant Rohila <jrohila55@gmail.com>
2026-04-06 13:21:01 +0530

    feat(analytics): add @vercel/analytics package and integrate into layout
    Introduces the @vercel/analytics package to enhance user interaction tracking. The Analytics component is integrated into the RootLayout, allowing for improved insights into user behavior within the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import "@/shared/styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/next";
 import { TRPCReactProvider } from "@/core/api/api.client";
 import RazorpayProvider from "@/core/payment/razorpay.provider";
 import { ThemeProvider } from "@/core/theme/theme.provider";
 import { Toaster } from "@/shared/components/ui/sonner";
 import { TooltipProvider } from "@/shared/components/ui/tooltip";
 import { className, viewport } from "@/shared/utils/methods/font";
-
 export { viewport };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
@@ -16,7 +16,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         <TRPCReactProvider>
           <ThemeProvider>
             <TooltipProvider>
-              <RazorpayProvider>{children}</RazorpayProvider>
+              <RazorpayProvider>
+                {children}
+                <Analytics />
+              </RazorpayProvider>
             </TooltipProvider>
             <Toaster />
           </ThemeProvider>
